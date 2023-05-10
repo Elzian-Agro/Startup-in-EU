45047f9726ab9504f27b2219ff7d70db01802508
Suresh_Gobi <sureshgobi34@gmail.com>
2023-05-10 10:52:20 +0530

    Sector part responsive updated

--- assets/js/slicker-slide.js
+++ assets/js/slicker-slide.js
@@ -1,6 +1,15 @@
 $('.slider').slick({
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-  });
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
